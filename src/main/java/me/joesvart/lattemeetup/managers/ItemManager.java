88bb17d7e0a51a7e10a8224d9d45f2e1c36aee5b
joesvart <joesvartt@gmail.com>
2021-01-06 20:13:06 -0300

    Update ItemManager
    Change the name of the items in the Lobby and in the code have better order.

--- src/main/java/me/joesvart/lattemeetup/managers/ItemManager.java
+++ src/main/java/me/joesvart/lattemeetup/managers/ItemManager.java
@@ -8,17 +8,39 @@ import org.bukkit.entity.Player;
 public class ItemManager {
     
     public void handleLobbyInventory(Player player) {
-        player.getInventory().setItem(0, new ItemBuilder(Material.BOOK).name(CC.GREEN + "Scenarios").build());
-        player.getInventory().setItem(7, new ItemBuilder(Material.EMERALD).name(CC.GREEN + "Stats").build());
-        player.getInventory().setItem(8, new ItemBuilder(Material.BED).name(CC.GREEN + "Leave to Lobby").build());
+        player.getInventory().setItem(0, new ItemBuilder(Material.BOOK)
+            .name(CC.GREEN + "Scenarios")
+            .lore(CC.GRAY + "Right click to vote for the scenarios.")
+            .build());
+
+        player.getInventory().setItem(7, new ItemBuilder(Material.EMERALD)
+            .name(CC.GREEN + "Leaderboards Menu")
+            .lore(CC.GRAY + "Right click to open the menu.")
+            .build());
+
+        player.getInventory().setItem(8, new ItemBuilder(Material.BED)
+            .name(CC.GREEN + "Leave to Lobby")
+            .lore(CC.GRAY + "Right click to return to the main Lobby.")
+            .build());
         
         player.updateInventory();
     }
 
     void handleSpectatorInventory(Player player) {
-        player.getInventory().setItem(0, new ItemBuilder(Material.ITEM_FRAME).name(CC.SECONDARY + "Spectate Menu").build());
-        player.getInventory().setItem(1, new ItemBuilder(Material.DIAMOND).name(CC.SECONDARY + "Random Teleport").build());
-        player.getInventory().setItem(8, new ItemBuilder(Material.COMPASS).name(CC.SECONDARY + "Navigation Compass").build());
+        player.getInventory().setItem(0, new ItemBuilder(Material.ITEM_FRAME)
+            .name(CC.AQUA + "Spectate Menu")
+            .lore(CC.GRAY + ("Right click to open the menu."))
+            .build());
+
+        player.getInventory().setItem(1, new ItemBuilder(Material.DIAMOND)
+            .name(CC.PRIMARY + "Random Teleport")
+            .lore(CC.GRAY + ("Right click to teleport to a random player."))
+            .build());
+
+        player.getInventory().setItem(8, new ItemBuilder(Material.COMPASS)
+            .name(CC.PINK + "Navigation Compass")
+            .lore(CC.GRAY + "Right click to teleport to that position.")
+            .build());
         
         player.updateInventory();
     }
