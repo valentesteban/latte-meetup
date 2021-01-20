74b797718b1270228f8331158b91c423bcf7a0e1
joesvart <joesvartt@gmail.com>
2021-01-20 17:29:20 -0300

    Update GamesPlayedButton.java

--- src/main/java/me/joesvart/lattemeetup/leaderboards/buttons/GamesPlayedButton.java
+++ src/main/java/me/joesvart/lattemeetup/leaderboards/buttons/GamesPlayedButton.java
@@ -1,28 +1,38 @@
 package me.joesvart.lattemeetup.leaderboards.buttons;
 
+import lombok.Getter;
 import me.joesvart.lattelibs.chat.ChatUtils;
 import me.joesvart.lattelibs.item.ItemCreator;
 import me.joesvart.lattelibs.menu.Button;
+import me.joesvart.lattemeetup.LatteMeetup;
 import me.joesvart.lattemeetup.player.PlayerData;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GamesPlayedButton extends Button {
+
+    @Getter
+    private LatteMeetup plugin = LatteMeetup.getInstance();
 
     @Override
     public ItemStack getButtonItem(Player player) {
         PlayerData playerData = PlayerData.getByName(player.getName());
 
+        List<String> lore = new ArrayList<>();
+        for (String played : ChatUtils.translate(plugin.getLeaderboardsConfig().getStringList("GAMES-PLAYED-BUTTON.LORE"))) {
+            played = played.replaceAll("<games-played>", String.valueOf(playerData.getPlayed()));
+
+            lore.add(played);
+        }
+
         return new ItemCreator(Material.EXP_BOTTLE)
-            .name(ChatUtils.translate("&2Your games"))
-            .lore(ChatUtils.translate("&7&m--------------------------"))
-            .lore(ChatUtils.translate("&7Games played: &f") + playerData.getPlayed())
-            .lore(ChatUtils.translate(""))
-            .lore(ChatUtils.translate("&8The statistics will be updated"))
-            .lore(ChatUtils.translate("&8after every game."))
-            .lore(ChatUtils.translate("&7&m--------------------------"))
+            .name(ChatUtils.translate(plugin.getLeaderboardsConfig().getString("GAMES-PLAYED-BUTTON.NAME")))
+            .lore(lore)
             .build();
     }
 
