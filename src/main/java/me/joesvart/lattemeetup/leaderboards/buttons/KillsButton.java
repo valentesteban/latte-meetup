7b078d18d43a0e961859a24be4385ebfa0d1c3da
joesvart <joesvartt@gmail.com>
2021-01-20 17:29:23 -0300

    Update KillsButton.java

--- src/main/java/me/joesvart/lattemeetup/leaderboards/buttons/KillsButton.java
+++ src/main/java/me/joesvart/lattemeetup/leaderboards/buttons/KillsButton.java
@@ -1,28 +1,38 @@
 package me.joesvart.lattemeetup.leaderboards.buttons;
 
+import lombok.Getter;
 import me.joesvart.lattelibs.chat.ChatUtils;
 import me.joesvart.lattelibs.item.ItemCreator;
 import me.joesvart.lattelibs.menu.Button;
+import me.joesvart.lattemeetup.LatteMeetup;
 import me.joesvart.lattemeetup.player.PlayerData;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class KillsButton extends Button {
+
+    @Getter
+    private LatteMeetup plugin = LatteMeetup.getInstance();
 
     @Override
     public ItemStack getButtonItem(Player player) {
         PlayerData playerData = PlayerData.getByName(player.getName());
 
+        List<String> lore = new ArrayList<>();
+        for (String kills : ChatUtils.translate(plugin.getLeaderboardsConfig().getStringList("KILLS-BUTTON.LORE"))) {
+            kills = kills.replaceAll("<kills>", String.valueOf(playerData.getKills()));
+
+            lore.add(kills);
+        }
+
         return new ItemCreator(Material.IRON_SWORD)
-            .name(ChatUtils.translate("&2Your kills"))
-            .lore(ChatUtils.translate("&7&m--------------------------"))
-            .lore(ChatUtils.translate("&7Kills: &f") + playerData.getKills())
-            .lore(ChatUtils.translate(""))
-            .lore(ChatUtils.translate("&8The statistics will be updated"))
-            .lore(ChatUtils.translate("&8after every game."))
-            .lore(ChatUtils.translate("&7&m--------------------------"))
+            .name(ChatUtils.translate(plugin.getLeaderboardsConfig().getString("KILLS-BUTTON.NAME")))
+            .lore(lore)
             .build();
     }
 
