b8adbc48bb5e9ec6a5708b19c06de70bf1e00425
joesvart <joesvartt@gmail.com>
2021-01-20 17:29:25 -0300

    Update WinsButton.java

--- src/main/java/me/joesvart/lattemeetup/leaderboards/buttons/WinsButton.java
+++ src/main/java/me/joesvart/lattemeetup/leaderboards/buttons/WinsButton.java
@@ -1,28 +1,38 @@
 package me.joesvart.lattemeetup.leaderboards.buttons;
 
+import lombok.Getter;
 import me.joesvart.lattelibs.chat.ChatUtils;
 import me.joesvart.lattelibs.item.ItemCreator;
 import me.joesvart.lattelibs.menu.Button;
+import me.joesvart.lattemeetup.LatteMeetup;
 import me.joesvart.lattemeetup.player.PlayerData;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WinsButton extends Button {
+
+    @Getter
+    private LatteMeetup plugin = LatteMeetup.getInstance();
 
     @Override
     public ItemStack getButtonItem(Player player) {
         PlayerData playerData = PlayerData.getByName(player.getName());
 
+        List<String> lore = new ArrayList<>();
+        for (String wins : ChatUtils.translate(plugin.getLeaderboardsConfig().getStringList("WINS-BUTTON.LORE"))) {
+            wins = wins.replaceAll("<wins>", String.valueOf(playerData.getWins()));
+
+            lore.add(wins);
+        }
+
         return new ItemCreator(Material.GOLDEN_APPLE)
-            .name(ChatUtils.translate("&2Your wins"))
-            .lore(ChatUtils.translate("&7&m--------------------------"))
-            .lore(ChatUtils.translate("&7Wins: &f") + playerData.getWins())
-            .lore(ChatUtils.translate(""))
-            .lore(ChatUtils.translate("&8The statistics will be updated"))
-            .lore(ChatUtils.translate("&8after every game."))
-            .lore(ChatUtils.translate("&7&m--------------------------"))
+            .name(ChatUtils.translate(plugin.getLeaderboardsConfig().getString("WINS-BUTTON.NAME")))
+            .lore(lore)
             .build();
     }
 
